99e6343a356d80404475379de95eddf702f669c1
nubik132 <mister.nubik@list.ru>
2023-04-17 22:32:21 +0300

    fix: binary tree's find and has methods are working now (Monday 17.04.2023 22:32:16)

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -31,19 +31,23 @@ class BinarySearchTree {
     }
   }
 
-  has(value) {//исправить undefined на [undefined, currentNode]
+  has(value) {
     if (Array.isArray(this.get(value, () => false))) return true;
     return false;
   }
 
-  find(value) {//исправить undefined на [undefined, currentNode] 
-    if (Array.isArray(this.get(value, () => 0))) return this.get(value, () => 0)[1];
+  find(value) {
+    if (Array.isArray(this.get(value, () => 0))) return this.get(value, () => 0)[2];
     else return null;
   }
 
   remove(value) {
     let array = this.get(value, () => { false });
     if (Array.isArray(array)) {
+      if (array[1] == null) {
+        this.rootNode = null;
+        return;
+      };
       let prev = array[1];
       if (value == prev.right.data)
         prev.right = null;
@@ -61,7 +65,7 @@ class BinarySearchTree {
   }
 
   get(value, func) {
-    var previousNode = null;
+    let previousNode = null;
     let currentNode = this.rootNode;
     while (currentNode !== null) {
       if (value > currentNode.data) {
@@ -72,20 +76,24 @@ class BinarySearchTree {
         previousNode = currentNode;
         currentNode = currentNode.left;
       }
-      else return [undefined, previousNode];
+      else return [undefined, previousNode, currentNode];
     }
     return func(currentNode, previousNode);
   }
 }
 
-// const tree = new BinarySearchTree();
-// tree.add(1);
-// tree.add(2);
-// tree.add(3);
-// tree.add(4);
-// tree.add(5);
-// console.log(tree);
-// console.log(tree.min());
+const tree = new BinarySearchTree();
+tree.add(9);
+tree.add(14);
+tree.add(54);
+tree.add(2);
+tree.add(6);
+tree.add(8);
+tree.add(31);
+tree.add(1);
+tree.remove(6);
+tree.remove(2);
+console.log(tree.min(), 1);
 // console.log(tree.max());
 // tree.remove(5);
 // console.log(tree.has(5));
